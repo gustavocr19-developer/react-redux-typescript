ea8ec3dbf729a45176677da2a49922bf5982acad
gustavocr19-developer <gustavocr.developer@gmail.com>
2020-12-31 00:32:19 -0300

    worked well

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { removePost } from './redux/actions/PostActions';
 import { getPosts } from './redux/effects/Posts';
 import { Post } from './redux/interfaces/Post';
 import { AppState } from './redux/store';
@@ -9,15 +10,22 @@ import { AppState } from './redux/store';
 const App = () => {
   
   const dispatch = useDispatch()
+  const [ids,setIds] = useState(0)
 
   useEffect(() => {
     dispatch(getPosts())
   }, [dispatch])  
 
   const posts = useSelector((state:AppState) => state.posts)
+  const removeItem = () =>{
+    dispatch(removePost(ids))
+    setIds(ids+1)
+  }
+
 
   return (
    <>
+    <button onClick={removeItem}>Remove </button>
     {posts.posts.map((post:Post)=> {
       return(
         <div key={post.id}>
