96f086cdcb4ea0ebcf406955ea08a2d70cb9ca75
gustavocr19-developer <gustavocr.developer@gmail.com>
2020-12-31 00:11:59 -0300

    posts done

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { getPosts } from './redux/effects/Posts';
+import { Post } from './redux/interfaces/Post';
+import { AppState } from './redux/store';
+
+
+const App = () => {
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])  
+
+  const posts = useSelector((state:AppState) => state.posts)
+
+  return (
+   <>
+    {posts.posts.map((post:Post)=> {
+      return(
+        <div>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+        )
+    })}
+   </>
+  );
+}
+
+export default App
